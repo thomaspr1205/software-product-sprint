699616670a5b4ba33f9deefd37e9b722ff2f4127
Thomas <thomasfelipepr@gmail.com>
2021-03-18 22:54:49 +0000

    fix indentation

--- portfolio/src/main/java/com/google/sps/servlets/Chat.java
+++ portfolio/src/main/java/com/google/sps/servlets/Chat.java
@@ -11,30 +11,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/chat")
 public class Chat extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html;");
-        String jsonQuestions = convertToJsonUsingGson(randomQuestion());
-        response.getWriter().println(jsonQuestions);
-    }
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+    String jsonQuestions = convertToJsonUsingGson(randomQuestion());
+    response.getWriter().println(jsonQuestions);
+  }
 
-    public String[] randomQuestion() {
-        String[] questions = {
-            "Are there any interesting things your name spells with the letters rearranged?",
-            "If you were a potato, what way would you like to be cooked?",
-            "Would you go to space if you knew that you could never come back to earth?",
-            "Have you ever been mistaken for someone famous?",
-            "What animal would you chose to be?",
-            "What is the most embarrassing thing you have ever done?",
-            "What is the strangest gift you have ever received?",
-            "What kind of reality show would you appear in?",
-            "What song describes your life right now?"
-        };
-        return questions;
-    }
-    private String convertToJsonUsingGson(String[] questions) {
-        Gson gson = new Gson();
-        String jsonQuestions = gson.toJson(questions);
-        return jsonQuestions;
-    }
+  public String[] randomQuestion() {
+    String[] questions = {
+      "Are there any interesting things your name spells with the letters rearranged?",
+      "If you were a potato, what way would you like to be cooked?",
+      "Would you go to space if you knew that you could never come back to earth?",
+      "Have you ever been mistaken for someone famous?",
+      "What animal would you chose to be?",
+      "What is the most embarrassing thing you have ever done?",
+      "What is the strangest gift you have ever received?",
+      "What kind of reality show would you appear in?",
+      "What song describes your life right now?"
+    };
+    return questions;
+  }
+  private String convertToJsonUsingGson(String[] questions) {
+    Gson gson = new Gson();
+    String jsonQuestions = gson.toJson(questions);
+    return jsonQuestions;
+  }
 }
--- portfolio/src/main/webapp/scriptMainPage.js
+++ portfolio/src/main/webapp/scriptMainPage.js
@@ -1,54 +1,54 @@
 var slideIndex = 1;
 
 function onClickGithub() {
-    window.open("https://github.com/thomaspr1205");
+  window.open("https://github.com/thomaspr1205");
 }
 
 function onClickLinkedin() {
-    window.open("https://www.linkedin.com/in/thomaspenar");
+  window.open("https://www.linkedin.com/in/thomaspenar");
 }
 
 function onClickProject3() {
-    document.getElementById("project3").style.display = "block";
+  document.getElementById("project3").style.display = "block";
 }
 
 async function showRandomQuestion() {
-    const responseFromServer = await fetch('/chat');
-    const questionList = await responseFromServer.json();
+  const responseFromServer = await fetch('/chat');
+  const questionList = await responseFromServer.json();
 
-    const questionsContainer = document.getElementById('question-container');
-    randomIndex = Math.floor(Math.random() * 9) + 0;
-    randomQuestion = questionList[randomIndex];
-    questionsContainer.innerText = randomQuestion;
+  const questionsContainer = document.getElementById('question-container');
+  randomIndex = Math.floor(Math.random() * 9) + 0;
+  randomQuestion = questionList[randomIndex];
+  questionsContainer.innerText = randomQuestion;
 }
 
 // Accordions, when clicked it opens or close the option selected
 function myAccFunc(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
 }
 
 // Slideshow, keeps track of the slide that is being displayed
 function plusDivs(n) {
-    slideIndex = slideIndex + n;
-    showDivs(slideIndex);
+  slideIndex = slideIndex + n;
+  showDivs(slideIndex);
 }
 
 // Allows to view a slide and move to the next and previous slide.
 function showDivs(n) {
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = x.length
-    };
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = x.length
+  };
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
